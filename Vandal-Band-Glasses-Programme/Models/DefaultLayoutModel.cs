457052015598e97b87169900f32d2ac9f76f6f3f
Nathan Vassell <vass1881@vandals.uidaho.edu>
2022-10-17 16:27:41 -0700

    smol update

--- Vandal-Band-Glasses-Programme/Models/DefaultLayoutModel.cs
+++ Vandal-Band-Glasses-Programme/Models/DefaultLayoutModel.cs
@@ -5,7 +5,7 @@ public class DefaultLayoutModel
     public List<string> participantNames { get; set; } = new List<string>();
     public int numRows { get; set; } = 0;
     public int numColumns { get; set; } = 0;
-    public string customLayoutDirectory { get; set; } = "./Custom_Layouts/";
+    public string customLayoutDirectory { get; set; } = "/Custom_Layouts/";
 
     //public DefaultLayoutModel(List<string> names, int rows, int columns, string layoutDirectory)
     //{
